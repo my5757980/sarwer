cf835fb5526ee0f59e5d9ca9b4cf82983cc3f432
Muhammad yaseen <my5757980@gmail.com>
2025-02-11 12:35:34 +0500

    add to console

--- src/app/Product/page.tsx
+++ src/app/Product/page.tsx
@@ -8,8 +8,7 @@ interface books {
 
 import { client } from '@/sanity/lib/client';
 import Image from "next/image"
-import { BsCartDash } from "react-icons/bs";
-import Link from 'next/link';
+import ProductListing from '../components/product-listing/product-listing';
 
 
 
@@ -57,25 +56,7 @@ export default async function Product() {
             data.map((elem) => {
               return (
                 <div key={elem.id}>
-
-
-
-
-
-                  <div className="  hover:bg-blue-100 transition-transform transform hover:scale-110  duration-500 shadow-xl md:w-[230px]  w-[225px]">
-                    <Link href={`/productdetails/${elem._id}`}>
-                      <Image className="  " src={elem.imageUrl} alt={elem.title} width={220} height={312} />
-                      <div className="flex">
-                        <p className="text-[#007580] w-[145px] ">{elem.title}</p>
-                        <BsCartDash className="  w-[44px] h-[44px] rounded-[5px] bg-[#029FAE] relative left-[33px] top-[10px] text-white " />
-                      </div>
-                      <h1 className="relative bottom-[20px]  ">${elem.price}</h1>
-                      <h1 className=" relative bottom-[270px] left-[15px] bg-[#01AD5A] w-[49px] h-[26px] text-center text-white rounded-[4px] ">New</h1>
-                    </Link>
-                  </div>
-
-
-
+                <ProductListing  elem ={elem}/>
                 </div>
               )
             })
--- src/app/components/our-product/our-product.tsx
+++ src/app/components/our-product/our-product.tsx
@@ -9,9 +9,9 @@ interface books {
 
 
 
-import Image from "next/image"
-import { BsCartDash } from "react-icons/bs";
+
 import { client } from '@/sanity/lib/client';
+import ProductListing from '../product-listing/product-listing';
 
 
 
@@ -27,10 +27,7 @@ export default async function Product() {
   console.log(data)
     return (
         <div className="overflow-x-hidden">
-
-
-
-              
+        {/* Heading */}
       <div>
         <p className="    text-center text-[20px] font-bold pt-[44px] pb-[22px] lg:text-[30px]   ">Our Product</p>
       </div>
@@ -39,45 +36,28 @@ export default async function Product() {
 
       {/* All products */}
 
-
-
-
-
-
-
       <div className="w-[220px] m-auto  md:w-[450px] md:m-auto lg:m-auto lg:w-[1000px]    ">
         <div className=" flex flex-col gap-16 md:grid md:gap-5 md:grid-cols-2 lg:grid lg:gap-5 lg:grid-cols-4 ">
+
+
           {
+            
             data.slice(0,8).map((elem) => {
               return (
                 <div key={elem.id}>
-
-
-
-
-
-                  <div className="  hover:bg-blue-100 transition-transform transform hover:scale-110  duration-500 shadow-xl md:w-[230px]  w-[225px] mb-[33px]">
-    
-                      <Image className="  " src={elem.imageUrl} alt={elem.title} width={220} height={312} />
-                      <div className="flex">
-                        <p className="text-[#007580] w-[145px] ">{elem.title}</p>
-                        <BsCartDash className="  w-[44px] h-[44px] rounded-[5px] bg-[#029FAE] relative left-[33px] top-[10px] text-white " />
-                      </div>
-                      <h1 className="relative bottom-[20px]  ">${elem.price}</h1>
-                      <h1 className=" relative bottom-[270px] left-[15px] bg-[#01AD5A] w-[49px] h-[26px] text-center text-white rounded-[4px] ">New</h1>
                 
-                  </div>
-
-
-
-                </div>
+               <ProductListing  elem ={elem}/>
+               </div>
               )
             })
           }
+          
 
         </div>
       
+      
       </div>
+    
 
 
 
